750ff98bc54229f2210761b01f80963e00408193
hiro <hiromhiro617@gmail.com>
2022-07-15 11:41:52 +0900

    add socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const socket = new WebSocket('ws://localhost:4567/ws');
+
+const App = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [voltages, setVoltages] = useState<number[]>([]);
+  const [inputText, setInputText] = useState('');
+  const [isCounting, setIsCounting] = useState(false);
+
+  useEffect(() => {
+    if (!isCounting) return;
+    const id = setInterval(() => {
+      const num = Math.random();
+      socket.send(JSON.stringify({ type: 'voltageData', data: num }));
+    }, 1000);
+
+    return () => {
+      console.log('clear');
+      clearInterval(id);
+    };
+  }, [isCounting]);
+
+  socket.onopen = () => {
+    console.log('socket connected');
+    console.log(socket);
+  };
+  socket.onmessage = (event) => {
+    if (!event.data) {
+      return;
+    }
+    const message = JSON.parse(event.data);
+    if (message.type === 'voltageData') {
+      setVoltages([...voltages, message.data]);
+      return;
+    }
+    if (message.type === 'message') {
+      setMessages([...messages, message.data]);
+    }
+  };
+
+  const sendMessage = () => {
+    if (!socket) return;
+    socket.send(JSON.stringify({ type: 'message', data: inputText }));
+    setInputText('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <input
+          value={inputText}
+          onChange={(event) => setInputText(event.target.value)}
+        />
+        <button onClick={sendMessage}>send</button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setIsCounting(!isCounting);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {!isCounting ? 'start' : 'stop'}
+        </button>
+        <button
+          onClick={() => {
+            setMessages([]);
+          }}
+        >
+          clear
+        </button>
+      </div>
+      {messages.map((message, i) => (
+        <div key={i}>{message}</div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
